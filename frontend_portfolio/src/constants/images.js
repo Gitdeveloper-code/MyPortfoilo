bb5280704d6cdf9e67f01c631b59e46aae06e2e8
KasimAhmed <itdeveloper.coder@gmail.com>
2023-09-18 10:52:44 +0530

    first commit.

---
+++ frontend_portfolio/src/constants/images.js
@@ -0,0 +1,89 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import api from '../assets/api.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import flutter from '../assets/flutter.png';
+import git from '../assets/git.png';
+import graphql from '../assets/graphql.png';
+import html from '../assets/html.png';
+import java from '../assets/java.png';
+import kotlin from '../assets/kotlin.png';
+import android from '../assets/android.png';
+
+import javascript from '../assets/javascript.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+import vue from '../assets/vue.png';
+
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import mainlogo from '../assets/mainlogo.png';
+
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+import asus from '../assets/asus.png';
+import bolt from '../assets/bolt.png';
+import nb from '../assets/nb.png';
+import skype from '../assets/skype.png';
+import spotify from '../assets/spotify.png';
+
+import test1 from '../assets/test1.png';
+import test2 from '../assets/test2.png';
+import test3 from '../assets/test3.png';
+import test4 from '../assets/test4.png';
+
+
+export default {
+  email,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  git,
+  graphql,
+  html,
+  javascript,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  vue,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
+  mainlogo,
+  adidas,
+  amazon,
+  asus,
+  bolt,
+  nb,
+  skype,
+  spotify,
+  java,
+  kotlin,
+  android,
+  test1,
+  test2,
+  test3,
+  test4
+};
